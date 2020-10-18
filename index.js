d39ed5aff5abf1f8a98d7762ba1c82cd6a774afe
vdonoladev <victordonolaf@gmail.com>
2020-10-18 09:36:48 -0300

    Updates

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Evandro - O cara das redes sociais
 let counter = 0;
-let arr = document.querySelectorAll('.L3NKy');
+let arr = document.querySelectorAll(".L3NKy");
 arr.forEach((v, i) => {
-    setTimeout(()=>{
-        if(!v.classList.contains('_8A5w5')){
-            v.click();
-            counter++;
-            console.log('Evandro já seguiu'+counter+'pessoas');
-        } else {
-            console.log('Você já segue este usuário.');
-                }
-    }, i * 10000 )
+  setTimeout(() => {
+    if (!v.classList.contains("_8A5w5")) {
+      v.click();
+      counter++;
+      console.log("Evandro já seguiu" + counter + "pessoas");
+    } else {
+      console.log("Você já segue este usuário.");
+    }
+  }, i * 10000);
 });
